ba6fcfc3c8f7c47512d6ca16e519305e5c3a8307
App Academy Student <student@appacademy.io>
2013-12-02 10:27:39 -0800

    Added display of board.

--- board.rb
+++ board.rb
@@ -1,21 +1,27 @@
+# -*- coding: utf-8 -*-
 class Board
-	attr_accessor :grid
-	
+	BLANK = "_|"
+  attr_accessor :grid
+
 	def initialize
 		fill_board
 	end
-	
+
 	def display
+    disp = ""
 		@grid.map do |row|
+      disp += "\n"
 			row.map do |piece|
-				piece.nil? ? piece : "_"
-			end.join
-		end.join("\n")
+        disp += piece.nil? ? BLANK : piece.render
+			end
+    end
+
+    puts disp
 	end
-	
+
 	def fill_board
 		@grid = Array.new(8) {Array.new(8)}
-		
+
 		8.times do |row|
 			8.times do |col|
 				if (row+col)%2 == 0 && row < 3
@@ -29,33 +35,33 @@ class Board
 
 end
 
-# -*- coding: utf-8 -*-
+
 class Piece
 	attr_reader :color, :board
 	attr_accessor :position
-	
+
 	def initialize(board, position, color)
 		@board = board
 		@position = position
 		@color = color
 	end
-	
+
 	def symbols
-		{ :white => 'w', :black => 'b' }
+		{ :white => '♙|', :black => '♟|' }
 	end
-	
+
 	def perform_slide
-	
+
 	end
-	
+
 	def perform_jump
-	
+
 	end
-	
+
 	def move_diffs
-	
+
 	end
-	
+
 	def render
 		symbols[color]
 	end
